0827d46dfa4fbe72f70056efe5eaa9bad402969b
Piyush <ppdpiyush@gmail.com>
2020-03-19 19:28:02 +0530

    minor push

--- screens/Order.js
+++ screens/Order.js
@@ -15,33 +15,34 @@ import BottomResults from "../components/BottomResults";
 import { SafeAreaView } from "react-navigation";
 import DeviceInfo from 'react-native-device-info';
 import ApplicationStyles from "../common/ApplicationStyles";
-
-
 const { Provider, Droppable, Draggable } = createDndContext();
 class TestScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      Variables:["pts per possession offense","pts per possession defense","home/away/neutral","days between games","line/pts given","temp","team quality","free throws per game"],
-      Selection:["Drop here","Drop here","Drop here"],
-      opacity:[]
     };
   }
   static navigationOptions =(props)=>({
     header:null
 });
-  getPercentage(){
-    return Math.floor(Math.random() * 101)+"%"; 
-  }
-  componentDidMount(){
-    this.setState({
-      opacity:Array(this.state.Variables.length).fill().map((_, i) => 1)
-    })
-  }
   render() {
     return (
       <SafeAreaView style={{flex:1,backgroundColor:'#ededed'}}>
-      <Text>Orders</Text>
+        
+      <View style={{flexDirection:'row',justifyContent:'space-between',marginHorizontal:nw(90),marginTop:nh(20)}}>
+        <View>
+        <View>
+        <Text>Trident Canteen</Text>
+      </View>
+            <Image source={Images.food} style={{height:nh(50),width:nw(50)}}/>
+        </View>
+        <View>
+          <Text>Food</Text>
+          <Text>Delivery Time</Text>
+          <Text>12:20</Text>
+        </View>
+      </View>
+      
       </SafeAreaView>
     );
   }
